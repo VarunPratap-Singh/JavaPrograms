8c9f884a1dc1a9dea9686b042682d7bb4dc987c5
VarunPratap-Singh <varunsingh9999@gmail.com>
2024-10-10 01:50:39 +0530

    Assignment22

---
+++ Class19_Abstract2.java
@@ -0,0 +1,28 @@
+package basicsprogram;
+
+class class10
+{
+	abstract void method10();
+	abstract void method11();
+	void method12()
+	{
+		
+	}
+}
+class class11 extends class10
+{
+	abstract void method13();
+	abstract void method14();
+}
+
+
+
+public class Class19_Abstract2 extends class11{
+
+	public static void main(String[] args)
+	{
+		
+
+	}
+
+}
