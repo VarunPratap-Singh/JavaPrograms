677322127c288e49c647d6a236fabb0a67893aea
VarunPratap-Singh <varunsingh9999@gmail.com>
2024-10-15 01:16:36 +0530

    Assignment24

---
+++ class21.java
@@ -0,0 +1,13 @@
+package Class21_AccessSpecifierCase4_1;
+import Class21_AccessSpecifierCase4.class22;
+
+public class class21 
+{
+	public static void main(String[] args) 
+	{
+		class22 c1= new class22();
+		c1.add();
+		
+	}
+
+}
---
+++ class22.java
@@ -0,0 +1,24 @@
+package Class21_AccessSpecifierCase4;
+
+
+public class class22
+{
+	public void add()
+	{
+		System.out.println("Class 21 AccessSpecifier Case4 public method packageClass214");
+		
+	}
+	private void div()
+	{
+	
+	}
+	protected void mul()
+	{
+	
+	}
+	 void sub()
+	{
+	
+	}
+
+}
