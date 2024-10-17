826358944ad0dcab85746cf41bbe2f93d32d4801
VarunPratap-Singh <varunsingh9999@gmail.com>
2024-10-17 10:43:58 +0530

    Assignment31

---
+++ Assignment31_Nullarray.java
@@ -0,0 +1,16 @@
+//Write an array program to check whether it stores null value or not
+package basicsprogram;
+
+public class Assignment31_Nullarray {
+
+	public static void main(String[] args) 
+	{
+		String s1[]= new String[5];
+		for (int i = 0; i < s1.length; i++) 
+		{
+			System.out.println("Value at index  : "+ i + " "+s1[i]);
+		}
+
+	}
+
+}
