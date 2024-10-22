3a5db49837d20de03df37b4b8806b00b5757cf78
VarunPratap-Singh <varunsingh9999@gmail.com>
2024-10-23 02:11:03 +0530

    Assignment42

---
+++ Assignment42_replaceAll5.java
@@ -0,0 +1,17 @@
+//WAP from the given string input replace all the Capital letters with Nothing.
+//
+//
+//Example: String name= "I am The Boss";
+package basicsprogram;
+
+public class Assignment42_replaceAll5 {
+
+	public static void main(String[] args)
+	{
+		String n1="Varun Pratap Singh";
+		String n2=n1.replaceAll("[A-Z]", "");
+		System.out.println(n2);
+
+	}
+
+}
