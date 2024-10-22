90187f886d0cb7a56272e537b42fc72b1bc48d01
VarunPratap-Singh <varunsingh9999@gmail.com>
2024-10-23 00:31:51 +0530

    Assignment37

---
+++ Assignment37_equalsIgnore.java
@@ -0,0 +1,14 @@
+//WAP to compare the String by using the method equalIgnoreCase.
+package basicsprogram;
+
+public class Assignment37_equalsIgnore {
+
+	public static void main(String[] args) 
+	{
+		String name="Varun";
+		boolean b1=name.equalsIgnoreCase("varun");
+		System.out.println(b1);
+
+	}
+
+}
