90ee10b004097168d7745f5e4e2299e6dd5eae0c
VarunPratap-Singh <varunsingh9999@gmail.com>
2024-10-23 00:52:20 +0530

    Assignment40

---
+++ Assignment40_replace3.java
@@ -0,0 +1,16 @@
+
+//WAP where from  the given string input replace the 2 with Nothing.
+//
+//Example: String name= "kv no 2";
+package basicsprogram;
+
+public class Assignment40_replace3 {
+
+	public static void main(String[] args) 
+	{
+		String name="Kv no 2";
+		String n1 = name.replace("2","");
+		System.out.println(n1);
+	}
+
+}
