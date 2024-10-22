544ec4b16ca655a65046fe54da61713139b5f927
VarunPratap-Singh <varunsingh9999@gmail.com>
2024-10-23 00:45:01 +0530

    Assignment38

---
+++ Assignment38_replace.java
@@ -0,0 +1,18 @@
+//WAP where from the given string replace the K with T.
+//Example: String name= "Kv no 2";
+
+package basicsprogram;
+
+public class Assignment38_replace {
+
+	public static void main(String[] args)
+	{
+		String name="Kv no 2";
+		String n1 = name.replace('K', 't');
+		System.out.println(n1);
+		
+		
+
+	}
+
+}
