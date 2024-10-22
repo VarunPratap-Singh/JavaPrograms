3bb2417d52b88ba9ae1bbf7fd8c6cd290457454a
VarunPratap-Singh <varunsingh9999@gmail.com>
2024-10-23 00:51:38 +0530

    Assignment39

---
+++ Assignment39_replace2.java
@@ -0,0 +1,16 @@
+//WAP where from the given string replace the City name Gurgaon to Delhi.
+//Example: String name= "I'm in Gurgaon";
+
+package basicsprogram;
+
+public class Assignment39_replace2 {
+
+	public static void main(String[] args)
+	{
+		String n1="I'm in Gurgaon";
+		String n2= n1.replace("Gurgaon", "Delhi");
+		System.out.println(n2);
+
+	}
+
+}
